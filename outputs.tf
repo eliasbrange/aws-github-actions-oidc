86dc94c424e9d9a03a9a8a7da761d84b9301daad
Elias Brange <elias.brange@gmail.com>
2022-04-08 19:01:22 +0200

    update roles

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "pull_request_role_arn" {
-  value = module.pull_request_role.role_arn
+output "read_role_arn" {
+  value = module.read_role.role_arn
 }
-output "main_branch_role_arn" {
-  value = module.main_branch_role.role_arn
+output "dev_deploy_role_arn" {
+  value = module.dev_deploy_role.role_arn
 }
-output "prod_env_role_arn" {
-  value = module.prod_env_role.role_arn
+output "prod_deploy_role_arn" {
+  value = module.prod_deploy_role.role_arn
 }
--- roles.tf
+++ roles.tf
@@ -1,7 +1,7 @@
-module "pull_request_role" {
+module "read_role" {
   source = "./oidc-role"
 
-  name = "gh-oidc-pull-request-role"
+  name = "gh-oidc-read-role"
   subject_claims = [
     "repo:eliasbrange/aws-github-actions-oidc:pull_request",
     "repo:eliasbrange/aws-github-actions-oidc:ref:refs/heads/main"
@@ -34,12 +34,12 @@ module "pull_request_role" {
   })
 }
 
-module "main_branch_role" {
+module "dev_deploy_role" {
   source = "./oidc-role"
 
-  name = "gh-oidc-main-branch-role"
+  name = "gh-oidc-dev-deploy-role"
   subject_claims = [
-    "repo:eliasbrange/aws-github-actions-oidc:ref:refs/heads/main"
+    "repo:eliasbrange/aws-github-actions-oidc:environment:Development"
   ]
   oidc_provider_arn = data.aws_iam_openid_connect_provider.github.arn
   policy = jsonencode({
@@ -52,22 +52,12 @@ module "main_branch_role" {
         ],
         "Resource" : [
           aws_s3_bucket.dev_env_bucket.arn,
-          aws_s3_bucket.prod_env_bucket.arn,
         ]
       },
       {
         "Effect" : "Allow",
         "Action" : [
           "s3:GetObject",
-        ],
-        "Resource" : [
-          "${aws_s3_bucket.dev_env_bucket.arn}/*",
-          "${aws_s3_bucket.prod_env_bucket.arn}/*",
-        ]
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
           "s3:PutObject",
         ],
         "Resource" : [
@@ -78,10 +68,10 @@ module "main_branch_role" {
   })
 }
 
-module "prod_env_role" {
+module "prod_deploy_role" {
   source = "./oidc-role"
 
-  name = "gh-oidc-prod-env-role"
+  name = "gh-oidc-prod-deploy-role"
   subject_claims = [
     "repo:eliasbrange/aws-github-actions-oidc:environment:Production"
   ]
