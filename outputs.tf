15e00ca8b99efd2cbd45022ff369e24cda2ad299
Elias Brange <elias.brange@gmail.com>
2022-04-08 17:28:06 +0200

    more roles

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,9 @@
-
+output "pull_request_role_arn" {
+  value = module.pull_request_role.role_arn
+}
+output "main_branch_role_arn" {
+  value = module.main_branch_role.role_arn
+}
+output "prod_env_role_arn" {
+  value = module.prod_env_role.role_arn
+}
--- roles.tf
+++ roles.tf
@@ -32,3 +32,81 @@ module "pull_request_role" {
     ]
   })
 }
+
+module "main_branch_role" {
+  source = "./oidc-role"
+
+  name = "gh-oidc-main-branch-role"
+  subject_claims = [
+    "repo:eliasbrange/aws-github-actions-oidc:ref:refs/heads/main"
+  ]
+  oidc_provider_arn = data.aws_iam_openid_connect_provider.github.arn
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:ListBucket"
+        ],
+        "Resource" : [
+          aws_s3_bucket.dev_env_bucket.arn,
+          aws_s3_bucket.prod_env_bucket.arn,
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:GetObject",
+        ],
+        "Resource" : [
+          "${aws_s3_bucket.dev_env_bucket.arn}/*",
+          "${aws_s3_bucket.prod_env_bucket.arn}/*",
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:PutObject",
+        ],
+        "Resource" : [
+          "${aws_s3_bucket.dev_env_bucket.arn}/*",
+        ]
+      }
+    ]
+  })
+}
+
+module "prod_env_role" {
+  source = "./oidc-role"
+
+  name = "gh-oidc-prod-env-role"
+  subject_claims = [
+    "repo:eliasbrange/aws-github-actions-oidc:environment:Production"
+  ]
+  oidc_provider_arn = data.aws_iam_openid_connect_provider.github.arn
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:ListBucket"
+        ],
+        "Resource" : [
+          aws_s3_bucket.prod_env_bucket.arn,
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:GetObject",
+          "s3:PutObject",
+        ],
+        "Resource" : [
+          "${aws_s3_bucket.prod_env_bucket.arn}/*",
+        ]
+      }
+    ]
+  })
+}
